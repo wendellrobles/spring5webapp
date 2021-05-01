f76549c465901ef10017e39becfb8f5a7dac676e
Wendell Robles <wendellrobles@yahoo.com>
2021-05-01 05:52:45 -0500

    Updated

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,8 +3,6 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 5/16/17.
- */
 public interface BookRepository extends CrudRepository<Book, Long> {
+
 }
